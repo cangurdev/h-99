a500fd10b079473bb4c136fb07a67227270b31fb
cvngur <cangur2398@gmail.com>
2021-01-27 02:38:29 +0300

    added questions 19,20

--- Questions11to20.hs
+++ Questions11to20.hs
@@ -37,3 +37,20 @@ slice :: [a] -> Int -> Int -> [a]
 slice (x:xs) k j
         | k > 2 = slice xs (k-1) (j-1)
         | otherwise = take (j-k+1) xs
+
+-- Problem 19
+-- (**) Rotate a list N places to the left.
+
+rotate :: [a] -> Int -> [a]
+rotate [] _ = []
+rotate x 0 = x
+rotate x y 
+   | y > 0 = rotate (tail x ++ [head x]) (y-1)
+   | otherwise = rotate (last x : init x) (y+1)
+
+-- Problem 20
+-- (*) Remove the K'th element from a list.
+
+removeAt 1 (x:xs) = (x,xs)
+removeAt n (x:xs) = (l,x:r)
+    where (l,r) = removeAt (n-1) xs 
